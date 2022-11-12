2bd6ffec86f406da9b34ce8e33e02c43d452d5b1
Xiantai01 <56475837+Xiantai01@users.noreply.github.com>
2022-11-12 15:01:49 +0800

    Update defectpyramid_train.py

--- defectpyramid_train.py
+++ defectpyramid_train.py
@@ -26,21 +26,18 @@ def create_model(num_classes, load_pretrain_weights=True):
                                      norm_layer = None,
                                   trainable_layers=3,
                                      Simsiam=False)
-    # 训练自己数据集时不要修改这里的91，修改的是传入的num_classes参数
     model = FasterRCNN(backbone=backbone, num_classes=91)
 
     if load_pretrain_weights:
-        # 载入预训练模型权重
-        # https://download.pytorch.org/models/fasterrcnn_resnet50_fpn_coco-258fb6c6.pth
         weights_dict = torch.load("./backbone/fasterrcnn_resnet50_fpn_coco.pth", map_location='cpu')
         missing_keys, unexpected_keys = model.load_state_dict(weights_dict, strict=False)
         if len(missing_keys) != 0 or len(unexpected_keys) != 0:
             print("missing_keys: ", missing_keys)
             print("unexpected_keys: ", unexpected_keys)
 
-    # get number of input features for the classifier
+    
     in_features = model.roi_heads.box_predictor.cls_score.in_features
-    # replace the pre-trained head with a new one
+    
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
 
     return model
@@ -50,7 +47,7 @@ def main(args):
     device = torch.device(args.device if torch.cuda.is_available() else "cpu")
     print("Using {} device training.".format(device.type))
 
-    # 用来保存coco_info的文件
+   
     results_file = "results{}.txt".format(datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
 
     data_transform = {
@@ -70,22 +67,19 @@ def main(args):
     train_dataset = NEU_DataSet(data_root, data_transform["train"], "train_1.txt")
     train_sampler = None
 
-    # 是否按图片相似高宽比采样图片组成batch
-    # 使用的话能够减小训练时所需GPU显存，默认使用
+    
     if args.aspect_ratio_group_factor >= 0:
         train_sampler = torch.utils.data.RandomSampler(train_dataset)
-        # 统计所有图像高宽比例在bins区间中的位置索引
+      
         group_ids = create_aspect_ratio_groups(train_dataset, k=args.aspect_ratio_group_factor)
-        # 每个batch图片从同一高宽比例区间中取
+       
         train_batch_sampler = GroupedBatchSampler(train_sampler, group_ids, args.batch_size)
 
-    # 注意这里的collate_fn是自定义的，因为读取的数据包括image和targets，不能直接使用默认的方法合成batch
     batch_size = args.batch_size
     #nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])  # number of workers
     nw = 0
     print('Using %g dataloader workers' % nw)
     if train_sampler:
-        # 如果按照图片高宽比采样图片，dataloader中需要使用batch_sampler
         train_data_loader = torch.utils.data.DataLoader(train_dataset,
                                                         batch_sampler=train_batch_sampler,
                                                         pin_memory=True,
@@ -98,8 +92,6 @@ def main(args):
                                                         pin_memory=True,
                                                         num_workers=nw,
                                                         collate_fn=train_dataset.collate_fn)
-
-    # load validation data set
 
     val_dataset = NEU_DataSet(data_root, data_transform["val"], "val_1.txt")
     val_data_set_loader = torch.utils.data.DataLoader(val_dataset,
@@ -124,12 +116,6 @@ def main(args):
 
     scaler = torch.cuda.amp.GradScaler() if args.amp else None
 
-
-    # lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
-    #                                                step_size=7,
-    #                                                gamma=0.33)
-    # lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer,
-    #                                                           T_max=2)
 
     if args.resume != "":
         checkpoint = torch.load(args.resume, map_location='cpu')
@@ -162,19 +148,11 @@ def main(args):
 
         # write into txt
         with open(results_file, "a") as f:
-            # 写入的数据包括coco指标还有loss和learning rate
             result_info = [f"{i:.4f}" for i in coco_info + [mean_loss.item()]] + [f"{lr:.6f}"]
             txt = "epoch:{} {}".format(epoch, '  '.join(result_info))
             f.write(txt + "\n")
 
         val_map.append(coco_info[1])  # pascal mAP
-
-        # save weights
-        # save_files = {
-        #     'model': model.state_dict(),
-        #     'optimizer': optimizer.state_dict(),
-        #     'lr_scheduler': lr_scheduler.state_dict(),
-        #     'epoch': epoch}
         save_files = {
             'model': model.state_dict(),
             'optimizer': optimizer.state_dict(),
@@ -200,8 +178,6 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(
         description=__doc__)
-
-    # 训练设备类型
     parser.add_argument('--device', default='cuda:0', help='device')
 
     parser.add_argument('--data-path', default='./NEU-DEF', help='dataset')
